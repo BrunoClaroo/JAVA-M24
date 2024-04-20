dbb1f8f56d64844c46f084af4eb6e302498d89c0
BrunoClaroo <bnoclaro@gmail.com>
2024-04-20 11:51:52 -0300

    GreenFoot

---
+++ GreenFoot/Atividade 1/Atividade1/Bee.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Classe abelha para ser utilizada no jogo BeeWorld.
+ * 
+ * @author Bruno Claro 
+ * @version 20/04/24
+ */
+public class Bee extends Actor
+{
+    /**
+     * Método que será executado quando apertado o botão act ou run. 
+     * 
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+        // se teclar para direita vira 10º
+        if (Greenfoot.isKeyDown("left")){
+            turn(-5);
+        }
+        // se tclar para direita vira 10º
+        if (Greenfoot.isKeyDown("right")){
+            turn(5);
+        }
+    }
+}
---
+++ GreenFoot/Atividade 1/Atividade1/MyWorld.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Bee bee = new Bee();
+        addObject(bee,58,54);
+        Spider spider = new Spider();
+        addObject(spider,512,97);
+        Fly fly = new Fly();
+        addObject(fly,32,210);
+        spider.setLocation(506,83);
+        spider.setLocation(569,326);
+        fly.setLocation(92,323);
+    }
+}
