1b972307263eed9b8fc837f1fb491c5fc85ba55d
BrunoClaroo <bnoclaro@gmail.com>
2024-04-27 10:08:09 -0300

    V2 27/04/24

--- GreenFoot/Atividade 1/Atividade1/Bee.java
+++ GreenFoot/Atividade 1/Atividade1/Bee.java
@@ -24,6 +24,7 @@ public class Bee extends Actor
         if (Greenfoot.isKeyDown("right")){
             turn(5);
         }
+        verificarPosicao();
     }
     /**
      * Método que verifica se está na direita do mundo 
@@ -34,6 +35,37 @@ public class Bee extends Actor
         }
         return false;
     }
-    
-    
+   /**
+     * Método que verifica se está na Esquerda do mundo 
+     */
+    public boolean isNaEsquerda(){
+        return(getX() < 10 );
+    }
+    /**
+     *  Método que verifica se está na base do mundo 
+     */
+    public boolean isNaBase(){
+        return(getY() > getWorld() . getHeight() -10 );
+    }
+     /**
+     * Método que verifica se está no topo do mundo 
+     */
+    public boolean isNoTopo(){
+        return(getY() < 10 );
+    }
+    /**
+     * método que verifica se a abelha está nos cantos e reposiciona ela 
+     */
+    public void verificarPosicao(){
+        //verificando se está na direita 
+        if(isNaDireita() ){
+            setLocation(10, getY());
+        }
+        //verificando se está na esquerda 
+         if(isNaEsquerda() ){
+            setLocation(790, getY());
+        }
+        //verificando se está no Topo 
+        //verificando se está na Base
+    }
 }
